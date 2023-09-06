101aa3afa2d39852b2e5a23ae4b4255c83cad4f8
Любаша <lyuba.shashina@inbox.ru>
2023-09-06 22:49:38 +0300

    Задание 2

--- task1/Program.cs
+++ task1/Program.cs
@@ -5,6 +5,7 @@ var a = Convert.ToInt32(a1);
 Console.WriteLine("Введите второе число: ");
 var b1 = Console.ReadLine();
 var b = Convert.ToInt32(b1);
+
 if (a >= b)
 {
     Console.WriteLine(a + " - больше");
---
+++ task2/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите первое число: ");
+var a1 = Console.ReadLine(); 
+var a = Convert.ToInt32(a1);
+
+Console.WriteLine("Введите второе число: ");
+var b1 = Console.ReadLine();
+var b = Convert.ToInt32(b1);
+
+Console.WriteLine("Введите третье число: ");
+var c1 = Console.ReadLine(); 
+var c = Convert.ToInt32(c1);
+
+int max;
+if (a >= b)
+{
+    max = a;
+}
+else
+{
+    max = b;   
+}
+
+if (max >= c)
+{
+    Console.WriteLine(max + " - больше");
+}
+else
+{
+    Console.WriteLine(c + " - больше");   
+}
