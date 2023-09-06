4f94170dba78c781875a3600d3ea488bff4e96c7
Любаша <lyuba.shashina@inbox.ru>
2023-09-06 23:11:52 +0300

    Задача 3

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+var a1 = Console.ReadLine(); 
+var a = Convert.ToInt32(a1);
+
+if (a % 2 == 0)
+{
+    Console.WriteLine(a + " - чётное");
+}
+else
+{
+    Console.WriteLine(a + " - нечётное");
+}
