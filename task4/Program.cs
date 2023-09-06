c5bbb1e442ef380783c3fb42c1cd135e6aa7064c
Любаша <lyuba.shashina@inbox.ru>
2023-09-06 23:59:49 +0300

    Задача 4

---
+++ task4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+Console.WriteLine("Введите число");
+var n1 = Console.ReadLine();
+var n = Convert.ToInt32(n1);
+
+Console.WriteLine("Чётные числа:");
+for (var i = 1; i <= n; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
