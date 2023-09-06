576dd881f7ef992ead246db189088f2a14ddce8c
Любаша <lyuba.shashina@inbox.ru>
2023-09-06 22:18:56 +0300

    Задача 1

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число: ");
+var a1 = Console.ReadLine(); 
+var a = Convert.ToInt32(a1);
+
+Console.WriteLine("Введите второе число: ");
+var b1 = Console.ReadLine();
+var b = Convert.ToInt32(b1);
+if (a >= b)
+{
+    Console.WriteLine(a + " - больше");
+}
+else
+{
+    Console.WriteLine(b + " - больше");   
+}
